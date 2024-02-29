00b9e6c7e954b86cde49abcf1ff35dca6a045ad2
Tushar Prabhu <tusharprabhu26@gmail.com>
2024-03-01 03:46:56 +0530

    refactor: Update schema structure to remove duplicates
    without altering the behavior

--- evChargingSchema.js
+++ evChargingSchema.js
@@ -2,46 +2,31 @@ const mongoose = require('mongoose');
 const {Schema} = mongoose;
 
 // Connector Schema
+const LocationSubSchema = new Schema({
+  name: String,
+  location: {
+    type: {type: String, default: 'Point'},
+    coordinates: [Number],
+  },
+});
+
+LocationSubSchema.index({location: '2dsphere'});
+
 const ConnectorSchema = new Schema({
   type: String,
   wattage: Number,
   manufacturer: String,
   availability: Boolean,
-  chargePoint: {
-    name: String,
-    location: {
-      type: {type: String, default: 'Point'},
-      coordinates: [Number],
-    },
-  },
+  chargePoint: LocationSubSchema,
 });
 
-// Charge Point Schema
-const ChargePointSchema = new Schema({
-  name: String,
-  location: {
-    type: {type: String, default: 'Point'},
-    coordinates: [Number],
-  },
-});
-
-// Location Schema
-const LocationSchema = new Schema({
-  name: String,
-  location: {
-    type: {type: String, default: 'Point'},
-    coordinates: [Number],
-  },
-});
-
-// Create 2dsphere index
-ConnectorSchema.index({'chargePoint.location': '2dsphere'});
-ChargePointSchema.index({location: '2dsphere'});
-LocationSchema.index({location: '2dsphere'});
+const ChargePointSchema = LocationSubSchema;
+const LocationSchema = LocationSubSchema;
 
 // Models
 const Connector = mongoose.model('Connector', ConnectorSchema);
 const ChargePoint = mongoose.model('ChargePoint', ChargePointSchema);
 const Location = mongoose.model('Location', LocationSchema);
+
 
 module.exports = {Connector, ChargePoint, Location};
