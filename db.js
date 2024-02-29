32fb989bb89382c3db7ad9fe7a0a3dec5b9d8dce
Tushar Prabhu <tusharprabhu26@gmail.com>
2024-02-29 21:27:10 +0530

    fix: Test timeout error

--- db.js
+++ db.js
@@ -6,8 +6,8 @@ let mongoServer;
 async function connect() {
   let uri = 'mongodb://localhost:27017/charging_stations';
   if (process.env.NODE_ENV === 'development') {
-    mongoServer = new MongoMemoryServer();
-    uri = await mongoServer.getUri();
+    mongoServer = await MongoMemoryServer.create();
+    uri = mongoServer.getUri();
   }
   await mongoose.connect(uri);
 }
