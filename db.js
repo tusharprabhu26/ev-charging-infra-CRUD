f2bbe5ed5fd4f4e6e13aa3149cf070cf71b4d005
Tushar Prabhu <tusharprabhu26@gmail.com>
2024-03-01 03:12:58 +0530

    update: Remove URI switch

--- db.js
+++ db.js
@@ -1,14 +1,10 @@
 const mongoose = require('mongoose');
 const {MongoMemoryServer} = require('mongodb-memory-server');
 
-let mongoServer;
 
 async function connect() {
-  let uri = 'mongodb://localhost:27017/charging_stations';
-  if (process.env.NODE_ENV === 'development') {
-    mongoServer = await MongoMemoryServer.create();
-    uri = mongoServer.getUri();
-  }
+  const mongoServer = await MongoMemoryServer.create();
+  const uri = mongoServer.getUri();
   await mongoose.connect(uri);
 }
 
